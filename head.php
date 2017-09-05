6c782fc36fb2f6f0d808dc083c5be5d10a1b58a2
Deathshiver31 <jeffreydereus@gmail.com>
2017-09-05 14:13:07 +0200

    aDDED ROOSTER.PHP

--- head.php
+++ head.php
@@ -4,5 +4,6 @@
   <meta charset="utf-8">
   <title>MBIAO17A0</title>
   <link rel="stylesheet" type="text/css" href="global.css">
+  <link rel="shortcut icon" href=".ico">
 </head>
 <body>
---
+++ rooster.php
@@ -0,0 +1,12 @@
+<?php
+include_once "head.php";
+include_once "navbar.php";
+ ?>
+<br><br>
+ <iframe src="https://roosters.xedule.nl/Attendee/ScheduleCurrent/85920?Code=MBIAO17A0&attId=1&OreId=61" width="100%" height="800px"></iframe>
+
+
+
+<?php
+include_once "footer.php";
+ ?>
